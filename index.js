31ed27a35652991a28bc5c4d5c67ee18a259c6e5
wjchoi <wonjeung.choi@amorepacific.com>
2021-10-26 19:59:53 +0900

    V02

--- index.js
+++ index.js
@@ -1,5 +1,56 @@
-var express = require("express");
+function send_message(phone) {
+    var user_phone_number = phone;//수신 전화번호 기입
+    var resultCode = 404;
+    const date = Date.now().toString();
+    const uri = process.env.SERVICE_ID; //서비스 ID
+    const secretKey = process.env.NCP_SECRET_KEY;// Secret Key
+    const accessKey = process.env.NCP_KEY;//Access Key
+    const fromMobile = process.env.FROMMOBILE;
+    const method = "POST";
+    const space = " ";
+    const newLine = "\n";
+    const url = `https://sens.apigw.ntruss.com/sms/v2/services/${uri}/messages`;
+    const url2 = `/sms/v2/services/${uri}/messages`;
+    const hmac = CryptoJS.algo.HMAC.create(CryptoJS.algo.SHA256, secretKey);
+    hmac.update(method);
+    hmac.update(space);
+    hmac.update(url2);
+    hmac.update(newLine);
+    hmac.update(date);
+    hmac.update(newLine);
+    hmac.update(accessKey);
+    const hash = hmac.finalize();
+    const signature = hash.toString(CryptoJS.enc.Base64);
+    request({
+      method: method,
+      json: true,
+      uri: url,
+      headers: {
+        "Contenc-type": "application/json; charset=utf-8",
+        "x-ncp-iam-access-key": accessKey,
+        "x-ncp-apigw-timestamp": date,
+        "x-ncp-apigw-signature-v2": signature,
+      },
+      body: {
+        type: "SMS",
+        countryCode: "82",
+        from: fromMobile,
+        content: "wj-nodejs test",
+        messages: [
+          { to: `${user_phone_number}`, },],
+      },
+    },
+      function (err, res, html) {
+        if (err) console.log(err);
+        else { resultCode = 200; console.log(html); }
+      }
+    );
+    return resultCode;
+  }
+
+
 var app = express();
+
 
 app.set("port", (process.env.PORT || 3000));
 app.listen(app.get("port"),
@@ -9,4 +60,12 @@ function(){
 
 app.get("/", function(req, res){
     res.send("Hellooooooo");
+})
+
+app.get('/sms/:phone', (req, res) => {
+    console.log("==================================");
+    const paramObj = req.params;
+    send_message(paramObj.phone);
+    res.send("complete!");
+    console.log("send message done");
 })
