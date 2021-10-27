160023b38cf4dde8bb292050fca6dc7cf3ba6f03
wjchoi <wonjeung.choi@amorepacific.com>
2021-10-27 13:12:43 +0900

    pgV01

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ var CryptoJS = require('crypto-js');
 var request = require('request');
 
 function send_message(phone) {
-    var user_phone_number = phone;//수신 전화번호 기입
+    var user_phone_number = phone;
     var resultCode = 404;
     const date = Date.now().toString();
-    const uri = process.env.SERVICE_ID; //서비스 ID
-    const secretKey = process.env.NCP_SECRET_KEY;// Secret Key
-    const accessKey = process.env.NCP_KEY;//Access Key
+    const uri = process.env.SERVICE_ID;
+    const secretKey = process.env.NCP_SECRET_KEY;
+    const accessKey = process.env.NCP_KEY;
     const fromMobile = process.env.FROMMOBILE;
     const method = "POST";
     const space = " ";
@@ -73,3 +73,29 @@ app.get('/sms/:phone', (req, res) => {
     res.send("complete!");
     console.log("send message done");
 })
+
+const {Pool, Client} = require('pg');
+
+const pool = new Pool({
+  host: 'ec2-54-210-226-209.compute-1.amazonaws.com',
+  user: 'ctycjxwmmgvcdp',
+  password: '95f79433d60722bee720b279f7cc0388d4eac6f7dc20664a73726604a6b7e62a',
+  database: 'dbaa2eu926srs2',
+  port: 5432,
+  ssl: {},
+});
+
+pool.connect();
+
+pool.query('INSERT INTO target_send VALUES( FirstName, LastName, Mobile)', ['Wonjeung','Choi','01031248442'], (err, res) => {
+  console.log(res); // Hello World!
+  pool.end();
+});
+
+/*
+pool.query('SELECT * FROM target_send', (err, res) => {
+  if(!err) console.log(res);
+  else console.log(err);
+  pool.end();
+})
+*/
