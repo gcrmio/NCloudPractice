734233bcc4965dd16ce43f0d6fe4c5292b8331a5
wjchoi <wonjeung.choi@amorepacific.com>
2021-11-01 14:28:21 +0900

    GetResult - V01

--- index.js
+++ index.js
@@ -3,6 +3,43 @@ var CryptoJS = require('crypto-js');
 var request = require('request');
 var pg = require('pg');
 
+function get_result(messageId){
+  var message_id = messageId;
+  const uri = process.env.SERVICE_ID;
+  const secretKey = process.env.NCP_SECRET_KEY;
+  const accessKey = process.env.NCP_KEY;
+  const method = "GET";
+  const url = `https://sens.apigw.ntruss.com/sms/v2/services/${uri}/messages/${message_id}`;
+  const hmac = CryptoJS.algo.HMAC.create(CryptoJS.algo.SHA256, secretKey);
+  hmac.update(method);
+  hmac.update(space);
+  hmac.update(url);
+  hmac.update(newLine);
+  hmac.update(date);
+  hmac.update(newLine);
+  hmac.update(accessKey);
+  const hash = hmac.finalize();
+  const signature = hash.toString(CryptoJS.enc.Base64);
+  request({
+    method: method,
+    json: true,
+    uri: url,
+    headers: {
+      "Contenc-type": "application/json; charset=utf-8",
+      "x-ncp-iam-access-key": accessKey,
+      "x-ncp-apigw-timestamp": date,
+      "x-ncp-apigw-signature-v2": signature,
+    },
+  },
+    function (err, res, html) {
+      if (err) console.log(err);
+      else { resultCode = 200; console.log(html); }
+    }
+  );
+  return resultCode;
+}
+
+
 function send_message(mobile, sb, msg) {
     var user_phone_number = mobile;
     var resultCode = 404;
@@ -188,3 +225,12 @@ function dbSelect(){
   })
 }
 
+app.get('/getResult', (req, res) => {
+  try {
+    console.log("GETRESULT=======================================");
+    get_result();
+    res.send('GET RESULT Complete!');
+  } catch (error) {
+    console.log('There was an error!');
+  }
+})
