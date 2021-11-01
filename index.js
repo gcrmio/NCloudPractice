822e2c472be83d1c001311cf3c5741d4a8523cea
wjchoi <wonjeung.choi@amorepacific.com>
2021-11-01 15:02:38 +0900

    GetResult - V06

--- index.js
+++ index.js
@@ -2,101 +2,7 @@ var express = require('express');
 var CryptoJS = require('crypto-js');
 var request = require('request');
 var pg = require('pg');
-
-function get_result(messageId){
-  var message_id = messageId;
-  const uri = process.env.SERVICE_ID;
-  const secretKey = process.env.NCP_SECRET_KEY;
-  const accessKey = process.env.NCP_KEY;
-  const method = "POST";
-  const url = `https://sens.apigw.ntruss.com/sms/v2/services/${uri}/messages/${message_id}`;
-  const url2 = `/sms/v2/services/${uri}/messages`;
-    const hmac = CryptoJS.algo.HMAC.create(CryptoJS.algo.SHA256, secretKey);
-    hmac.update(method);
-    hmac.update(space);
-    hmac.update(url2);
-    hmac.update(newLine);
-    hmac.update(date);
-    hmac.update(newLine);
-    hmac.update(accessKey);
-    const hash = hmac.finalize();
-    const signature = hash.toString(CryptoJS.enc.Base64);
-  
-  request({
-    method: method,
-    json: true,
-    uri: url,
-    headers: {
-      "Content-type": "application/json; charset=utf-8",
-      "x-ncp-iam-access-key": accessKey,
-      "x-ncp-apigw-timestamp": date,
-      "x-ncp-apigw-signature-v2": signature,
-    },
-    body: {},
-  },
-    function (err, res, html) {
-      if (err) console.log(err);
-      else { resultCode = 200; console.log(html); }
-    }
-  );
-  return resultCode;
-}
-
-
-function send_message(mobile, sb, msg) {
-    var user_phone_number = mobile;
-    var resultCode = 404;
-    const date = Date.now().toString();
-    const uri = process.env.SERVICE_ID;
-    const secretKey = process.env.NCP_SECRET_KEY;
-    const accessKey = process.env.NCP_KEY;
-    const fromMobile = process.env.FROMMOBILE;
-    const method = "POST";
-    const space = " ";
-    const newLine = "\n";
-    const url = `https://sens.apigw.ntruss.com/sms/v2/services/${uri}/messages`;
-    const url2 = `/sms/v2/services/${uri}/messages`;
-    const hmac = CryptoJS.algo.HMAC.create(CryptoJS.algo.SHA256, secretKey);
-    hmac.update(method);
-    hmac.update(space);
-    hmac.update(url2);
-    hmac.update(newLine);
-    hmac.update(date);
-    hmac.update(newLine);
-    hmac.update(accessKey);
-    const hash = hmac.finalize();
-    const signature = hash.toString(CryptoJS.enc.Base64);
-    request({
-      method: method,
-      json: true,
-      uri: url,
-      headers: {
-        "Contenc-type": "application/json; charset=utf-8",
-        "x-ncp-iam-access-key": accessKey,
-        "x-ncp-apigw-timestamp": date,
-        "x-ncp-apigw-signature-v2": signature,
-      },
-      body: {
-        type: "LMS",
-        countryCode: "82",
-        from: fromMobile,
-        subject: sb,
-        content: msg,
-        messages: [
-          { to: `${user_phone_number}`, },],
-      },
-    },
-      function (err, res, html) {
-        if (err) console.log(err);
-        else { resultCode = 200; console.log(html); }
-      }
-    );
-    return resultCode;
-  }
-
-
 var app = express();
-
 
 app.set("port", (process.env.PORT || 3000));
 app.listen(app.get("port"),
@@ -111,16 +17,9 @@ app.get("/", function(req, res){
 // app.get('/sms/:phone', (req, res) => {
   app.get('/sms/', (req, res) => {
     console.log("==================================");
-    //const paramObj = req.params;
     dbSelect();
-
-    // for(const phone of mArr){
-    //   //send_message(paramObj.phone);
-    //   console.log('+++++++++++++++++++');
-    //   console.log(phone);
-      
-    //   send_message(phone);
-    // }
+    //const paramObj = req.params;
+    //send_message(paramObj.phone);
     res.send("complete!");
     console.log("send message done");
 })
@@ -156,18 +55,6 @@ app.get('/dbInsert', (req, res) => {
   }
 })
 
-function dbInsert(){
-  const sql = `INSERT INTO target_send(firstname, lastname, mobile) VALUES($1, $2, $3) RETURNING *`;
-  const values = ['Karl', 'Jung', '01071564823'];
-  client.query(sql, values, (err, res) => {
-    if(err){
-      console.log(err.stack);
-    } else {
-      console.log(res.rows[0]);
-    }
-  });
-}
-
 app.get('/dbSelect', (req, res) => {
   try {
     console.log("DBSELECT=======================================");
@@ -178,6 +65,14 @@ app.get('/dbSelect', (req, res) => {
   }
 })
 
+app.get('/status/:messageId', (req, res) => {
+    console.log("==================================");
+    const paramObj = req.params;
+    get_result(paramObj.phone);
+    res.send("complete!");
+    console.log("get send result done");
+})
+//0.DB쿼리조회
 function dbSelect(){
   const sql = `SELECT sid, firstname, lastname, mobile, sb, msg FROM target_send`
 
@@ -211,25 +106,120 @@ function dbSelect(){
         } catch (error) {
           console.log('send message error');
         }
-        
-
       }
       // console.log('*************************************************************');
       // console.log(res.rows);
-      
     }
-    // console.log('*************************************************************');
-    // console.log("fnArr="+fnArr);
-    // console.log("lnArr="+lnArr);
-    // console.log("mArr="+mArr);
-    // console.log("sbArr="+sbArr);
-    // console.log("msgArr="+msgArr);
-    // console.log('*************************************************************');
   })
 }
 
-app.get('/getResult/:messageId', (req, res) => {
-    console.log("GETRESULT=======================================");
-    const paramObj = req.params;
-    send_message(paramObj.messageId);
-})
+//1.메시지 발송
+function send_message(mobile, sb, msg) {
+  var user_phone_number = mobile;
+  var resultCode = 404;
+  const date = Date.now().toString();
+  const uri = process.env.SERVICE_ID;
+  const secretKey = process.env.NCP_SECRET_KEY;
+  const accessKey = process.env.NCP_KEY;
+  const fromMobile = process.env.FROMMOBILE;
+  const method = "POST";
+  const space = " ";
+  const newLine = "\n";
+  const url = `https://sens.apigw.ntruss.com/sms/v2/services/${uri}/messages`;
+  const url2 = `/sms/v2/services/${uri}/messages`;
+  const hmac = CryptoJS.algo.HMAC.create(CryptoJS.algo.SHA256, secretKey);
+  hmac.update(method);
+  hmac.update(space);
+  hmac.update(url2);
+  hmac.update(newLine);
+  hmac.update(date);
+  hmac.update(newLine);
+  hmac.update(accessKey);
+  const hash = hmac.finalize();
+  const signature = hash.toString(CryptoJS.enc.Base64);
+  request({
+    method: method,
+    json: true,
+    uri: url,
+    headers: {
+      "Contenc-type": "application/json; charset=utf-8",
+      "x-ncp-iam-access-key": accessKey,
+      "x-ncp-apigw-timestamp": date,
+      "x-ncp-apigw-signature-v2": signature,
+    },
+    body: {
+      type: "LMS",
+      countryCode: "82",
+      from: fromMobile,
+      subject: sb,
+      content: msg,
+      messages: [
+        { to: `${user_phone_number}`, },],
+    },
+  },
+    function (err, res, html) {
+      if (err) console.log(err);
+      else { resultCode = 200; console.log(html); }
+    }
+  );
+  return resultCode;
+}
+
+//2. DB데이터추가
+function dbInsert(){
+  const sql = `INSERT INTO target_send(firstname, lastname, mobile) VALUES($1, $2, $3) RETURNING *`;
+  const values = ['Karl', 'Jung', '01071564823'];
+  client.query(sql, values, (err, res) => {
+    if(err){
+      console.log(err.stack);
+    } else {
+      console.log(res.rows[0]);
+    }
+  });
+}
+
+//3.메시지 발송 결과 조회
+function get_result(messageId) {
+  var message_id = messageId;
+  var resultCode = 404;
+  const date = Date.now().toString();
+  const uri = process.env.SERVICE_ID;
+  const secretKey = process.env.NCP_SECRET_KEY;
+  const accessKey = process.env.NCP_KEY;
+  const fromMobile = process.env.FROMMOBILE;
+  const method = "GET";
+  const space = " ";
+  const newLine = "\n";
+  const url = `https://sens.apigw.ntruss.com/sms/v2/services/${uri}/messages/${message_id}`;
+  const url2 = `/sms/v2/services/${uri}/messages/${message_id}`;
+  const hmac = CryptoJS.algo.HMAC.create(CryptoJS.algo.SHA256, secretKey);
+  hmac.update(method);
+  hmac.update(space);
+  hmac.update(url2);
+  hmac.update(newLine);
+  hmac.update(date);
+  hmac.update(newLine);
+  hmac.update(accessKey);
+  const hash = hmac.finalize();
+  const signature = hash.toString(CryptoJS.enc.Base64);
+  request({
+    method: method,
+    json: true,
+    uri: url,
+    headers: {
+      "Contenc-type": "application/json; charset=utf-8",
+      "x-ncp-iam-access-key": accessKey,
+      "x-ncp-apigw-timestamp": date,
+      "x-ncp-apigw-signature-v2": signature,
+    },
+    body: {
+
+    },
+  },
+    function (err, res, html) {
+      if (err) console.log(err);
+      else { resultCode = 200; console.log(html); }
+    }
+  );
+  return resultCode;
+}
