d6256ef454158fcc66c1ead1df759a0f59d6f505
wjchoi <wonjeung.choi@amorepacific.com>
2021-11-01 14:33:35 +0900

    GetResult - V02

--- index.js
+++ index.js
@@ -10,22 +10,13 @@ function get_result(messageId){
   const accessKey = process.env.NCP_KEY;
   const method = "GET";
   const url = `https://sens.apigw.ntruss.com/sms/v2/services/${uri}/messages/${message_id}`;
-  const hmac = CryptoJS.algo.HMAC.create(CryptoJS.algo.SHA256, secretKey);
-  hmac.update(method);
-  hmac.update(space);
-  hmac.update(url);
-  hmac.update(newLine);
-  hmac.update(date);
-  hmac.update(newLine);
-  hmac.update(accessKey);
-  const hash = hmac.finalize();
-  const signature = hash.toString(CryptoJS.enc.Base64);
+  
   request({
     method: method,
     json: true,
     uri: url,
     headers: {
-      "Contenc-type": "application/json; charset=utf-8",
+      "Content-type": "application/json; charset=utf-8",
       "x-ncp-iam-access-key": accessKey,
       "x-ncp-apigw-timestamp": date,
       "x-ncp-apigw-signature-v2": signature,
